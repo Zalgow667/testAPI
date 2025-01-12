307f4a13fb8a2b81f89ca23eb1304bfc25023393
Zalgow <gaetan.lepley@unicaen.fr>
2025-01-12 14:03:08 +0100

    end

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log("test")
+const express = require("express")
+const port = 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use("/", require("./routes/test.routes"))
+
+app.listen(port, () => console.log("Test sur le port : " + port))
+
